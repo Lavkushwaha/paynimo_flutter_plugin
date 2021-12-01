9ba352abaeef7e3c93bc587c8ef4f10904e19c0d
Lavkant Kachhwaha <kush373@gmail.com>
2021-12-01 10:43:11 +0530

    flutter paynimo class added working

--- android/src/main/java/com/example/flutter_paynimo/FlutterPaynimoPlugin.java
+++ android/src/main/java/com/example/flutter_paynimo/FlutterPaynimoPlugin.java
@@ -11,6 +11,8 @@ import androidx.annotation.Nullable;
 
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -18,6 +20,8 @@ import io.flutter.plugin.common.MethodChannel.Result;
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 
 import android.content.ContextWrapper;
@@ -40,10 +44,11 @@ import com.paynimo.android.payment.util.Constant;
 
 
 import java.util.Date;
+import java.util.Stack;
 
 
 /** FlutterPaynimoPlugin */
-public class FlutterPaynimoPlugin implements FlutterPlugin, MethodCallHandler {
+public class FlutterPaynimoPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
 //public class FlutterPaynimoPlugin extends FlutterActivity {
 //    private static final String CHANNEL = "samples.flutter.dev/battery";
 //    public static final int REQUEST_CODE_PAYTM = 0x00;
@@ -53,15 +58,34 @@ public class FlutterPaynimoPlugin implements FlutterPlugin, MethodCallHandler {
 //  /
 //  / This local reference serves to register the plugin with the Flutter Engine and unregister it
 //  / when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
 
-    private static Activity activity;
-    public static final int REQUEST_CODE_PAYTM = 0x00;
+    private static final String CHANNEL = "flutter_paynimo";
+    private MethodChannel channel;
+  private static FlutterPaynimoPlugin instance;
+  private FlutterPaynimoDelegate delegate;
+
+    private Activity activity;
+
+
+
+
+  public static void registerWith(Registrar registrar) {
+    if(instance == null) {
+      instance = new FlutterPaynimoPlugin();
+    }
+
+    if(registrar.activity() != null) {
+      instance.onAttachedToEngine((FlutterPluginBinding) registrar.messenger());
+      instance.onAttachedToActivity(registrar.activity());
+      registrar.addActivityResultListener(instance.getActivityResultListener());
+    }
+
+  }
+
 
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-
 
 
     channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_paynimo");
@@ -71,11 +95,16 @@ public class FlutterPaynimoPlugin implements FlutterPlugin, MethodCallHandler {
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
 
+    if(activity == null || delegate == null) {
+      result.error("no_activity", "esewa_pnp plugin requires a foreground activity.", null);
+    }
+
 
     if (call.method.equals("startPayment")) {
       // result.success("Android " + android.os.Build.VERSION.RELEASE);
     //   startPay();
-       Log.d(TAG, "onMethodCall: ");
+      delegate.startPay(call,result);
+//       Log.d(TAG, "onMethodCall: ");
 
     } else {
       result.notImplemented();
@@ -86,14 +115,48 @@ public class FlutterPaynimoPlugin implements FlutterPlugin, MethodCallHandler {
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
+    channel.setMethodCallHandler(this);
+    channel= null;
+
   }
 
-    private void onAttachedToActivity(Activity activity) {
+  private PluginRegistry.ActivityResultListener getActivityResultListener() {
+    return delegate;
+  }
+
+  private void onAttachedToActivity(Activity activity) {
         this.activity = activity;
+        delegate = new FlutterPaynimoDelegate(activity);
     }
 
 
+  @Override
+  public void onAttachedToActivity(@NonNull ActivityPluginBinding activityPluginBinding) {
+    if(getActivityResultListener() != null) {
+      activityPluginBinding.removeActivityResultListener(getActivityResultListener());
+    }
+    onAttachedToActivity(activityPluginBinding.getActivity());
+    activityPluginBinding.addActivityResultListener(getActivityResultListener());
+  }
 
-  
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+  activity = null;
+  delegate =null;
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding activityPluginBinding) {
+    if(getActivityResultListener() != null) {
+      activityPluginBinding.removeActivityResultListener(getActivityResultListener());
+    }
+    onAttachedToActivity(activityPluginBinding.getActivity());
+    activityPluginBinding.addActivityResultListener(getActivityResultListener());
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+    activity = null;
+    delegate = null;
+  }
 }
